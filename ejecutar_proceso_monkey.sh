122278af3ea66d86c89067e30da7b054b9a1ddaf
mcgomeztuniandes <mc.gomezt1@uniandes.edu.co>
2022-05-27 17:06:23 -0500

    Update ejecutar_proceso_monkey.sh

--- ejecutar_proceso_monkey.sh
+++ ejecutar_proceso_monkey.sh
@@ -54,7 +54,7 @@ echo "*** Se eliminan las carpetas de Ghost ***"
 rm -rf $DIRECTORIOACTUAL/ghost_4.41.3
 rm -rf $DIRECTORIOACTUAL/ghost_4.30.0
 
-echo "*** Elimina la carpeta para poner las imagenes de la ejecucion de ghost 4.41.3***"
+echo "*** Elimina la carpeta para poner las evidencias de la ejecucion de ghost 4.41.3***"
 rm -rf $DIRECTORIOACTUAL/resultadoMonkey
 mkdir $DIRECTORIOACTUAL/resultadoMonkey
 cp -r $DIRECTORIOACTUAL/monkey-cypress/result $DIRECTORIOACTUAL/resultadoMonkey
